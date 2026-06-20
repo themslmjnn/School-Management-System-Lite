18d5b1cfb64e0bded93ec954cc0d6171537c64a4
Munavarov Muslimjon <mnvrv.mm@gmail.com>
2026-06-20 11:40:18 +0500

    Removed schema files

--- src/schemas/auth.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from src.utils.enums import UserRole
-
-
-class CreateAccessTokenRequest(BaseModel):
-    user_id: int
-    role: UserRole
-    access_token_version: int
