8f8cc7912feeabf55dffc34847fe9c1e527ad21c
Munavarov Muslimjon <mnvrv.mm@gmail.com>
2026-08-16 09:12:50 +0500

    -> Remove student subject enrollment table

--- src/academics/models/__init__.py
+++ src/academics/models/__init__.py
@@ -1,7 +1,5 @@
 from .head_of_class_assignment import HeadOfClassAssignment
-from .student_subject_enrollment import StudentSubjectEnrollment
 
 __all__ = [
-    "StudentSubjectEnrollment",
     "HeadOfClassAssignment",
 ]
