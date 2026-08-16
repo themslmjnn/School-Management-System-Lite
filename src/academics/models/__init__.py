e1bdc5080c4f7b1dfbcbe57f8d566cb8a454b622
Munavarov Muslimjon <mnvrv.mm@gmail.com>
2026-08-16 09:07:39 +0500

    -> Removed teaching assignments table

--- src/academics/models/__init__.py
+++ src/academics/models/__init__.py
@@ -1,9 +1,7 @@
 from .head_of_class_assignment import HeadOfClassAssignment
 from .student_subject_enrollment import StudentSubjectEnrollment
-from .teaching_assignment import TeachingAssignment
 
 __all__ = [
-    "TeachingAssignment",
     "StudentSubjectEnrollment",
     "HeadOfClassAssignment",
 ]
